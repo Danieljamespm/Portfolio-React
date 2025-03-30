80245d4d4e304103ef346a38aea09d5d5002e6da
Danny Mettler <danieljamespm@gmail.com>
2025-03-29 21:18:38 -0400

    got rid of scroll down button. I thought it was unnecessary.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
           <img src={ME} alt="Danny Mettler" />
         </div>
 
-        {/* <a href="#contact" className="scroll__down">Scroll Down</a> */}
+        
 
       </div>
     </header>
